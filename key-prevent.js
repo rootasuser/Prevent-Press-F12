f96bd53d1b4344487a6433b01d3e0a22d51b7134
Windel Navales <126387763+rootasuser@users.noreply.github.com>
2025-06-02 16:52:07 +0800

    Create key-prevent.js

---
+++ key-prevent.js
@@ -0,0 +1,7 @@
+$(document).keydown(function (event) {
+    if (event.keyCode == 123) { // Prevent F12
+        return false;
+    } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) { // Prevent Ctrl+Shift+I        
+        return false;
+    }
+});
